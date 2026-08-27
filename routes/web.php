1d4771c6b938810a6272a51d35e01032ad98a2b7
JasmineMarin <jasminemarin@student.trimexcolleges.edu.ph>
2026-08-27 09:53:22 +0800

    Refactor /courses route into CourseController@index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => view('home'));
+Route::get('/about', fn () => view('about'));
+Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
+Route::get('/contact', fn () => view('contact'));
