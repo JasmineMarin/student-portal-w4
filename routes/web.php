44e640ba6b68ce8c0ffc9fce42e8d3b8d3c71db9
JasmineMarin <jasminemarin@student.trimexcolleges.edu.ph>
2026-08-27 10:10:47 +0800

    Create StudentController resource controller and register students routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', fn () => view('home'));
 Route::get('/about', fn () => view('about'));
 Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
 Route::get('/courses/{code}', [CourseController::class, 'show'])->name('courses.show');
 Route::get('/contact', fn () => view('contact'));
+
+Route::resource('students', StudentController::class);
