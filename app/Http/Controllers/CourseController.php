57bed4b6281141ed324388cda7942389af2d973d
JasmineMarin <jasminemarin@student.trimexcolleges.edu.ph>
2026-08-28 13:54:48 +0800

    Refactor /courses route into CourseController@index

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -9,26 +9,10 @@ class CourseController extends Controller
     public function index()
     {
         $courses = [
-            ['code' => 'WEBDEV3', 'title' => 'Web Framework Laravel Development', 'units' => 5],
-            ['code' => 'DBMS2', 'title' => 'Advanced Database Systems', 'units' => 3],
-            ['code' => 'SE1', 'title' => 'Software Engineering 1', 'units' => 3],
+            ['code' => 'WEBDEV3', 'title' => 'Web Framework Laravel Development'],
+            ['code' => 'WEBDEV2', 'title' => 'Server-Side Scripting'],
         ];
 
-        return view('courses', ['courses' => $courses]);
-    }
-
-    public function show(string $code)
-    {
-        $courses = [
-            'WEBDEV3' => 'Web Framework Laravel Development',
-            'WEBDEV2' => 'Server-Side Scripting',
-        ];
-
-        abort_unless(isset($courses[$code]), 404);
-
-        return view('courses.show', [
-            'code' => $code,
-            'title' => $courses[$code],
-        ]);
+        return view('courses.index', ['courses' => $courses]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
-use App\Http\Controllers\StudentController;
 
 Route::get('/', fn () => view('home'));
 Route::get('/about', fn () => view('about'));
-Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
-Route::get('/courses/{code}', [CourseController::class, 'show'])->name('courses.show');
+Route::get('/courses', function () {
+    $courses = [
+        ['code' => 'WEBDEV3', 'title' => 'Web Framework Laravel Development', 'units' => 5],
+        ['code' => 'DBMS2', 'title' => 'Advanced Database Systems', 'units' => 3],
+        ['code' => 'SE1', 'title' => 'Software Engineering 1', 'units' => 3],
+    ];
+    return view('courses', ['courses' => $courses]);
+});
 Route::get('/contact', fn () => view('contact'));
-
-Route::resource('students', StudentController::class);
+Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
